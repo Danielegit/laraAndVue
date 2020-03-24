1d431b5934077df754488f93a2e816757fc81225
Daniele Argiolas <danieleargiolas9@gmail.com>
2020-03-24 08:40:22 +0100

    migrations ready

--- database/migrations/2020_03_24_071830_create_products_table.php
+++ database/migrations/2020_03_24_071830_create_products_table.php
@@ -15,6 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->double('price', 8, 2);
+            $table->unsignedBigInteger('company_id');
             $table->timestamps();
         });
     }
